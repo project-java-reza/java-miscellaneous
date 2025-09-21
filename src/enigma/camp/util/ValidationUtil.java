833913ab9595c65c9c2ef0158655a6a217fe950c
fullstack-jay <rizqirezaardiansyah@gmail.com>
2025-09-21 14:09:22 +0700

    [IMPROVE] implementation ValidationUtil

--- src/enigma/camp/util/ValidationUtil.java
+++ src/enigma/camp/util/ValidationUtil.java
@@ -1,5 +1,20 @@
 package enigma.camp.util;
 
+import enigma.camp.data.LoginRequest;
+import enigma.camp.error.ValidationException;
+
 public class ValidationUtil {
+
+    public static void validate(LoginRequest loginRequest) throws ValidationException {
+        if (loginRequest.username() == null) {
+            throw new NullPointerException("Username tidak boleh null");
+        } else if (loginRequest.username().isBlank()) {
+            throw new ValidationException("Username tidak boleh kosong");
+        } else if (loginRequest.password() == null) {
+            throw new NullPointerException("Password tidak boleh null");
+        } else if (loginRequest.password().isBlank()) {
+            throw new ValidationException("Password tidak boleh kosong");
+        }
+    }
 
 }
