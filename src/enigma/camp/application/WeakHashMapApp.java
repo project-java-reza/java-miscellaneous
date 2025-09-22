af3495ac6dd3bf2a9f3bf92475ac8b68d708f7fb
project-java-reza <rizqirezaardiansyahjava@gmail.com>
2025-09-22 13:55:02 +0700

    [IMPROVE]WeakHashMap implementation Map Collection

--- src/enigma/camp/application/WeakHashMapApp.java
+++ src/enigma/camp/application/WeakHashMapApp.java
@@ -1,13 +1,12 @@
 package enigma.camp.application;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.WeakHashMap;
 
 public class WeakHashMapApp {
     public static void main(String[] args) {
 
-        Map<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> map = new WeakHashMap<>();
 
         for (int i = 0; i < 1_000_000; i++) {
             map.put(i, i);
