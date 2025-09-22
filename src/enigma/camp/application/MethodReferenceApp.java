9a5fc7c5fde92e661295ad9e68af40efb6f065b6
project-java-reza <rizqirezaardiansyahjava@gmail.com>
2025-09-23 06:15:48 +0700

    [IMPROVE]MethodReference with method reference static, non static, Object, Parameter implementation Java Lambda Expression

--- src/enigma/camp/application/MethodReferenceApp.java
+++ src/enigma/camp/application/MethodReferenceApp.java
@@ -2,6 +2,7 @@ package enigma.camp.application;
 
 import enigma.camp.util.StringUtil;
 
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class MethodReferenceApp {
@@ -13,5 +14,37 @@ public class MethodReferenceApp {
         System.out.println(predicateIsLowerCase.test("reza")); // true
         System.out.println(predicateIsLowerCase.test("Reza")); // false
 
+        Function<String, String> function = String::toUpperCase;
+
+        System.out.println(function.apply("reza")); // REZA
+
+    }
+
+    public void run() {
+        // with method reference to non static method
+        Predicate<String> predicateIsLowerCase = this::isLowerCase;
+
+        System.out.println(predicateIsLowerCase.test("reza")); // true
+        System.out.println(predicateIsLowerCase.test("Reza")); // false
+    }
+
+    public void run2() {
+        MethodReferenceApp app = new MethodReferenceApp();
+
+        // with method reference to non static method
+        Predicate<String> predicateIsLowerCase = app::isLowerCase;
+
+        System.out.println(predicateIsLowerCase.test("reza")); // true
+        System.out.println(predicateIsLowerCase.test("Reza")); // false
+    }
+
+    // non static method
+    public boolean isLowerCase(String value) {
+        for (char c : value.toCharArray()) {
+            if (!Character.isLowerCase(c)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
