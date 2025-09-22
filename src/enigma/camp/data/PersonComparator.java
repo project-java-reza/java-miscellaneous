7d5cce47caa5620022d8ead2e66ac0a53b40fa79
project-java-reza <rizqirezaardiansyahjava@gmail.com>
2025-09-22 13:17:04 +0700

    [IMPROVE] PersonComparator implementation

---
+++ src/enigma/camp/data/PersonComparator.java
@@ -0,0 +1,11 @@
+package enigma.camp.data;
+
+import java.util.Comparator;
+
+public class PersonComparator implements Comparator<Person> {
+
+    @Override
+    public int compare(Person p1, Person p2) {
+        return p1.getName().compareTo(p2.getName());
+    }
+}
