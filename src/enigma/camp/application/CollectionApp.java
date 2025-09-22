6bca34fb7884edf3c7fc51f6d6af4c84cdc057bd
project-java-reza <rizqirezaardiansyah@gmail.com>
2025-09-22 07:46:13 +0700

    [IMPROVE] implementation collection

---
+++ src/enigma/camp/application/CollectionApp.java
@@ -0,0 +1,33 @@
+package enigma.camp.application;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class CollectionApp {
+    public static void main(String[] args) {
+        Collection<String> collection = new ArrayList<>();
+
+        collection.add("Rizqi");
+        collection.add("Reza");
+        collection.add("Ardiansyah");
+        collection.addAll(List.of("Ummu", "Sa'adah"));
+
+        for (var value : collection) {
+            System.out.println(value);
+        }
+
+        System.out.println("REMOVE");
+        collection.remove("Ardiansyah");
+        collection.removeAll(List.of("Rizqi", "Reza"));
+
+        for (var value : collection) {
+            System.out.println(value);
+        }
+
+        System.out.println(collection.contains("Ardiansyah"));
+        System.out.println(collection.containsAll(List.of("Ummu", "Sa'adah")));
+        // Im'Here
+        System.out.println(collection.size());
+    }
+}
