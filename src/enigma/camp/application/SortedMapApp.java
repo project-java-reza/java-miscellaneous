3551e236922058db52f75d4c8ea531ea315b5647
project-java-reza <rizqirezaardiansyahjava@gmail.com>
2025-09-22 14:23:52 +0700

    [IMPROVE]SortedMap with Comparator implementation Map Collection

--- src/enigma/camp/application/SortedMapApp.java
+++ src/enigma/camp/application/SortedMapApp.java
@@ -1,19 +1,28 @@
 package enigma.camp.application;
 
+import java.util.Comparator;
 import java.util.SortedMap;
 import java.util.TreeMap;
+
 
 public class SortedMapApp {
     public static void main(String[] args) {
 
-        SortedMap<String, String> sortedMap = new TreeMap<>();
+        Comparator<String> stringComparator = new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o2.compareTo(o1); // urut terbalik
+            }
+        };
+
+        SortedMap<String, String> sortedMap = new TreeMap<>(stringComparator);
 
         sortedMap.put("Reza", "Reza");
         sortedMap.put("Ummu", "Ummu");
         sortedMap.put("Ardiansyah", "Ardiansyah");
 
         for (var key : sortedMap.keySet()) {
-            System.out.println(key); // hasil terurut sesuai abjad
+            System.out.println(key); 
         }
 
     }
