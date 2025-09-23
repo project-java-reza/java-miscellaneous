e5638ae7813c4213eb75a58f59541e8fbd763c4e
project-java-reza <rizqirezaardiansyahjava@gmail.com>
2025-09-23 08:59:16 +0700

    [IMPROVE]Optional menghindari NullException dan get data di Optional implementation Java Lambda Expression

--- src/enigma/camp/application/OptionalApp.java
+++ src/enigma/camp/application/OptionalApp.java
@@ -13,9 +13,15 @@ public class OptionalApp {
     }
 
     public static void sayHello(String name) {
-        Optional.ofNullable(name)
-                // method reference
+//        Optional.ofNullable(name)
+//                // method reference
+//                .map(String::toUpperCase)
+//                .ifPresentOrElse(value -> System.out.println("HELLO " + value),
+//                        () -> System.out.println("HELLO"));
+
+        String upperName = Optional.ofNullable(name)
                 .map(String::toUpperCase)
-                .ifPresent(value -> System.out.println("HELLO " + value) );
+                .orElseGet(() -> "TEMAN");
+        System.out.println("HELLO " + upperName);
     }
 }
