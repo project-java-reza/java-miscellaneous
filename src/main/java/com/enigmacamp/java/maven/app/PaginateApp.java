1312ecbe18480f42246cafb252f317a19783e674
project-java-reza <rizqirezaardiansyahjava@gmail.com>
2025-09-23 14:25:42 +0700

    [EXERCISE] pagination

--- src/main/java/com/enigmacamp/java/maven/app/PaginateApp.java
+++ src/main/java/com/enigmacamp/java/maven/app/PaginateApp.java
@@ -5,20 +5,20 @@ import java.util.List;
 public class PaginateApp {
     public static void main(String[] args) {
         List<String> data = List.of("Apple", "Banana", "Cherry", "Durian", "Elderberry", "Fig");
-        int jumlahHalaman = 2;
-        paginate(data, jumlahHalaman, 1);
+        int jumlahDataHalaman = 2;
+        paginate(data, jumlahDataHalaman, 1);
 
     }
 
-    public static void paginate(List<String> data, int jumlahHalaman, int halamanParameter) {
-       int awalIndeks = (halamanParameter - 1) * jumlahHalaman;
-       int akhirIndeks = Math.min(awalIndeks + jumlahHalaman, data.size());
+    public static void paginate(List<String> data, int jumlahDataHalaman, int halamanDiminta) {
+       int awalIndeks = (halamanDiminta - 1) * jumlahDataHalaman;
+       int akhirIndeks = Math.min(awalIndeks + jumlahDataHalaman, data.size());
 
        if (awalIndeks >= data.size()) {
-              System.out.println("Halaman " + halamanParameter + ": Halaman tidak tersedia.");
+              System.out.println("Halaman " + halamanDiminta+ ": Halaman tidak tersedia.");
          } else {
               List<String> halaman = data.subList(awalIndeks, akhirIndeks);
-              System.out.println("Halaman " + halamanParameter + ": " + halaman);
+              System.out.println("Halaman " + halamanDiminta+ ": " + halaman);
        }
     }
 }
